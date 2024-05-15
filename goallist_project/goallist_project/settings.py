9270de452725d9dbe2602b87e3b9779161e2419e
aonori103 <aonori103@gmail.com>
2024-05-15 13:09:58 +0900

    update commit

--- goallist_project/goallist_project/settings.py
+++ goallist_project/goallist_project/settings.py
@@ -133,7 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (STATIC_DIR,)
+# STATICFILES_DIRS = (STATIC_DIR,)
+STATIC_ROOT =  os.path.join(BASE_DIR, "static")
 
 LOGIN_URL = '/accounts/user_login' #ログインしてないときにlogin_requiredのViewを開いたときのリダイレクト先View
 LOGIN_REDIRECT_URL = '/accounts/home' #LoginViewで遷移先が指定されていないときに遷移するURL
