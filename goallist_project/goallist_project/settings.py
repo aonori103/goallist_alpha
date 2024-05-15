67052c1ecbb76d1a7b005e9c12f8cdf6e07d7a98
aonori103 <aonori103@gmail.com>
2024-05-15 18:32:56 +0900

    local

--- goallist_project/goallist_project/settings.py
+++ goallist_project/goallist_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-e2=a1r5)xs998xu4mn@03hs032%*f=vhf25vq1omchchgkor7e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com', 'aonori103.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com', 'aonori103.pythonanywhere.com',]
 
 
 # Application definition
@@ -156,3 +156,8 @@ SESSION_COOKIE_SECURE = True
 
 from django.core.management.utils import get_random_secret_key
 SECRET_KEY = get_random_secret_key()  
+
+try:
+    from .local_settings import *
+except:
+    pass
